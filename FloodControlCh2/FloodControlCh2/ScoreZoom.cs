a25fbee94329966237b33f3ad41efdea48d7eb9e
Rfoder <G00233075@W205-16.academic.raritanval.edu>
2015-09-23 12:50:19 -0400

    Almost!!!
    almost done, finished typing. Now just have to debug...... oh no the
    horror!!!

---
+++ FloodControlCh2/FloodControlCh2/ScoreZoom.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace FloodControl
+{
+  public string Text;
+    public Color DrawColor;
+    private int displayCounter;
+    private int maxDisplayCount = 30;
+    private float scale = 0.4f;
+    private float lastScaleAmount = 0.0f;
+    private float scaleAmount = 0.4f
+
+    public float Scale
+{
+    get { return scaleAmount * displayCounter; }
+}
+    public bool IsCompleted
+{
+    get { return (displayCounter > maxDisplayCount); }
+}
+    public ScoreZoom (string displayText, Color fontColor)
+{
+    Text = displayText;
+    DrawColor = fontColor;
+    displayCounter = 0;
+}
+    public void Update()
+{
+    scale += lastScaleAmount + scaleAmount;
+    lastScaleAmount += scaleAmount;
+    displayCounter++;
+     }
+  }
+}
